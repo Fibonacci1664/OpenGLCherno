c2c79611d02af4b4f0d342e2ddfca7667a28745d
Fibonacci <dave.green1664@yahoo.co.uk>
2020-08-20 18:55:36 +0100

    Added a createShader and compileShader funcs. also wrote my first proper shader, woo hoo!

--- src/Application.cpp
+++ src/Application.cpp
@@ -2,7 +2,57 @@
 #include<GL/glew.h>
 #include <GLFW/glfw3.h>
 
+// READ ALL THE DOCS RELATED TO THESE FUNCS.
+static unsigned int compileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
 
+
+    // Error handling.
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    // If false it means the shader did NOT compile successfully.
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));           // This is dynamically creating on the STACK, noramally dynamic creation happens on the HEAP, save worrying about forgetting to DELETE it later on.
+
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : " frag") << '\n';
+        std::cout << message << '\n';
+
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+// READ ALL THE DOCS RELATED TO THESE FUNCS.
+static unsigned int createShader(const std::string& vertexShader, const std::string& fragShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+// READ ALL THE DOCS RELATED TO THESE FUNCS.
 int main(void)
 {
     GLFWwindow* window;
@@ -48,6 +98,29 @@ int main(void)
     // Define how many and the type of positions we have 1D, 2D, 3D, 4D, ints, floats etc.
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = createShader(vertexShader, fragShader);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -70,6 +143,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
 
